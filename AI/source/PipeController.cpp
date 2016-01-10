3edebe551730c39b2ee0eaf702c63eb73803ac22
Aaron Santiago <aaronsantiago@gmail.com>
2016-01-10 17:35:15 -0500

    Added support for piped input

---
+++ AI/source/PipeController.cpp
@@ -0,0 +1,46 @@
+#include "PipeController.hpp"
+
+#include <cmath>
+
+
+std::string PipeController::pipePath = "/home/aaron/pipe";
+
+PipeController::PipeController() {
+
+  namedPipe = new std::ofstream();
+  namedPipe->open (pipePath);
+
+}
+
+PipeController::~PipeController() {
+
+  namedPipe->close();
+
+}
+
+void PipeController::Press(char button) {
+  std::string output = "PRESS ";
+  *namedPipe << ((output + button) + "\n");
+}
+
+void PipeController::Release(char button) {
+  std::string output = "RELEASE ";
+  *namedPipe << ((output + button) + "\n");
+}  
+
+void PipeController::Stick(float rad, int deg, bool CStick) {
+
+  float x, y;
+
+  x = rad * cos (deg * M_PI / 180);
+  y = rad * sin (deg * M_PI / 180);
+
+  std::string output = "SET ";
+  output += (CStick ? "1 " : "0 ");
+  output += std::to_string(x);
+  output += " ";
+  output += std::to_string(y);
+
+  *namedPipe << (output + "\n");
+
+}
---
+++ AI/source/PipeController.hpp
@@ -0,0 +1,34 @@
+#ifndef PIPECONTROLLER_HPP
+#define PIPECONTROLLER_HPP
+
+#include "Controller.hpp"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+class PipeController : public Controller {
+
+private:
+
+  static std::string pipePath;
+  std::ofstream* namedPipe;
+
+public:
+
+  PipeController();
+  ~PipeController();
+  void Press(char);
+  void Release(char);    
+
+  /** 1st arg = 0 to 1, radius of stick
+      2nd arg = 0 to 360, angle of stick, pushed to the right = 0 degrees
+      3rd arg, true for c-stick, false for control stick
+  */
+  void Stick(float,int,bool=false);
+
+};
+    
+#endif
+        
+        
+        
