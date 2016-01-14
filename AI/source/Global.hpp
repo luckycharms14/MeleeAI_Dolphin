be5369b8ffe5f67a7d0e5e33b859a84742b3bf32
Aaron Santiago <aaronsantiago@gmail.com>
2016-01-13 19:15:21 -0500

    Fixes for OSX, removed MemShare from makefile

--- AI/source/Global.hpp
+++ AI/source/Global.hpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <chrono>
 #include <thread>
+#include <string>
 
 #define GLOBAL_SLEEP(x) std::this_thread::sleep_for(std::chrono::milliseconds(x));
 #define GLOBAL_SIGN(x) ((x > 0) - (x < 0))
